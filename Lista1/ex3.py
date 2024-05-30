ecfc5dc0f48a46853a3120ee9f09b06ffd6a56ce
Professor Igor <trabalho.computador.sjn@gmail.com>
2024-05-30 17:40:13 -0300

    main

--- Lista1/ex3.py
+++ Lista1/ex3.py
@@ -1,9 +1,11 @@
-def gerar_vetor_triangular_superior(matriz_adjacencia):
-    n_vertices = len(matriz_adjacencia)
-    vetor_triangular_superior = []
-    for i in range(n_vertices):
-        for j in range(i + 1, n_vertices):
-            vetor_triangular_superior.append(matriz_adjacencia[i][j])
-    return vetor_triangular_superior
+def triangular_superior_vetor(matriz):
+    n = len(matriz)
+    vetor_binario = []
+    
+    for i in range(n):
+        for j in range(i + 1, n):
+            vetor_binario.append(matriz[i][j])
+    
+    return vetor_binario
 
 
